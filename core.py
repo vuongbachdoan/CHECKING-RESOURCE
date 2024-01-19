1a7c1e0d2655d6d1ec7cdcddde47e8afeccadcca
vuongbachdoan <vuongbdde160256@fpt.edu.vn>
2024-01-19 12:51:25 +0700

    feat: modify code and push data to S3

--- core.py
+++ core.py
@@ -1,70 +1,137 @@
-import csv
 import boto3
 from termcolor import colored
+import datetime
 
 session = boto3.Session()
 
 class Core: 
-    def getAllResources(self):
-        # Get the list of available services
+    """
+    Class to interact with various AWS services, providing functions for:
+        - Listing and retrieving information about EC2 instances
+        - Listing S3 buckets and their details
+        - Listing RDS instances
+        - Listing VPCs
+        - Listing Lambda functions
+    """
+    ec2_client = boto3.client('ec2')
+    s3 = boto3.client('s3')
+    lambda_ = boto3.client('lambda')
+    show_ui=True
+
+    # Desc: set to print result to console or not
+    def set_show_ui(self, val):
+        self.show_ui = val
+
+    # Desc: get all aws services available
+    def get_all_resources(self):
         services = session.get_available_services()
         return services
+    
+    # Desc: get ec2 instances in a region
+    def get_ec2_in_single_region(self, region_name='us-east-1', instance_state=['pending', 'running', 'shutting-down', 'terminated', 'stopping', 'stopped']):
+        ec2_instances = boto3.resource('ec2', region_name)
+        return ec2_instances.instances.all()  
+    
+    # Desc: get regions work with ec2
+    def get_ec2_regions(self):
+        regions = self.ec2_client.describe_regions()['Regions']
+        return regions
+    
+    # Desc: get ec2 instance details
+    def get_ec2_instance_details(self, instance_id, region):
+        ec2 = boto3.resource('ec2', region)
+        instance_details = ec2.Instance(instance_id)
+        return instance_details
 
-    def getEC2Instances(self, type=['pending', 'running', 'shutting-down', 'terminated', 'stopping', 'stopped']):
-        ec2_client = boto3.client('ec2')
+    # Desc: CHECK EC2
+    def get_all_ec2_instances(self, type=['pending', 'running', 'shutting-down', 'terminated', 'stopping', 'stopped'], region='all'):
+        regions = [region]
+        if region == 'all':
+            regions = self.get_ec2_regions()
+        csv_content = "Region, Name, Id, Type, State, Launch Time"
+        for region in regions:
+            if self.show_ui:
+                print(colored('🌍 - ', 'cyan') + region['RegionName'])
+            ec2_instances = self.get_ec2_in_single_region(region['RegionName'])
+            for instance in ec2_instances:
+                try:
+                    instance = self.get_ec2_instance_details(instance.id, region['RegionName'])
+                    instance_name = None
+                    if instance.tags is not None:
+                        for tag in instance.tags:
+                            if tag['Key'] == 'Name':
+                                instance_name = tag['Value']
+                    instance_id = instance.id
+                    instance_type = instance.instance_type
+                    instance_state = instance.state['Name']
+                    instance_launch_time = instance.launch_time
+                    csv_content += f"{region}, {instance_name},{instance_id},{instance_type},{instance_state},{instance_launch_time}\n"
+                    print(colored('⚠️(warning):', 'red'), instance_name, instance_id, instance_type, instance_state, instance_launch_time)
+                except Exception as e:
+                    print(f"Error processing instance {instance.id}: {e}")
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.s3.put_object(
+            Body=csv_content,
+            Bucket='vuongbach-checking-services',
+            Key=f"ec2/{timestamp}/instance_info.csv"
+        )
 
-        # Retrieves all regions/endpoints that work with EC2    
-        response = ec2_client.describe_regions()
+    # Desc: get s3 bucket details
+    def get_s3_buckets_with_details(self, bucket):
+        csv_content = ""
+        csv_content += ""
+        bucket_name = bucket['Name']
+        creation_date = bucket['CreationDate'].strftime("%Y-%m-%d %H:%M:%S")
+        try:
+            owner_id = bucket['Owner']['ID']
+        except KeyError:
+            print(f"Owner information not directly available for bucket: {bucket_name}")
+            owner_id = None
+        try:
+            objects = self.s3.list_objects(Bucket=bucket_name)['Contents']
+            object_count = len(objects)
+        except KeyError:
+            objects = []
+            object_count = 0
+        csv_row = f"{bucket_name},{creation_date},{owner_id},{object_count}"
+        csv_content += csv_row + "\n"
+        return csv_content
+    
+    # Desc: put object to S3 bucket
+    def put_object_to_S3(self, content, bucket_name, key):
+        self.s3.put_object(
+            Body=content,
+            Bucket=bucket_name, 
+            Key=key 
+        )
 
-        print(colored('Checking EC2 instances ', 'yellow'))  # Yellow for visibility
-        for region in response['Regions']:
-            print(colored('🌍 - ', 'cyan') + region['RegionName'])  # Cyan for region names
-        
-            instances = boto3.resource('ec2', region_name=region['RegionName']).instances.filter(
-                Filters=[
-                    {
-                        'Name': 'instance-state-name',
-                        'Values': type
-                    }
-                ]
-            )
-
-            for instance in instances:
-                instance_name = self.get_instance_name(instance)
-                print(colored(f'Instance Name: {instance_name}, Instance ID: {instance.id}, Status: {type}', 'green'))
-
-    def get_instance_name(self, instance):
-        # Extract the 'Name' tag value from the instance
-        for tag in instance.tags or []:
-            if tag['Key'] == 'Name':
-                return tag['Value']
-        return "N/A"
-
-    def listS3Buckets(self):
-        print(colored('Checking S3 buckets ', 'yellow'))  # Yellow for visibility
+    # Desc: list all bucket
+    def get_s3_buckets(self):
+        print(colored('Checking S3 buckets ', 'yellow')) 
         s3 = boto3.client('s3')
         buckets = s3.list_buckets()
         return buckets['Buckets']
 
-    def listS3Objects(self, bucketName):
-        print(bucketName)
-
-    def listRDSInstances(self):
-        print(colored('Checking RDS ', 'yellow'))  # Yellow for visibility
+    # Desc: get rds instances
+    def get_rds_instances(self):
+        print(colored('Checking RDS ', 'yellow')) 
         rds = boto3.client('rds')
         return rds.describe_db_instances()['DBInstances']
+    
+    # Desc: get rds instance details
+    def get_rds_instance_detail(self, instance):
+        status_indicator = colored('⚠️ (running)', 'red') if instance['DBInstanceStatus'] == 'available' else 'Instance:'
+        print(status_indicator, instance['DBInstanceIdentifier'], instance['DBInstanceClass'], instance['Engine'])
+        return f"{instance['DBInstanceIdentifier']}, {instance['DBInstanceClass']}, {instance['Engine']}\n"
 
-    def listVPCs(self):
-        # ec2_resource = boto3.resource('ec2')
-        print(colored('Checking VPCs', 'yellow'))  # Yellow for visibility
-
-        allVpcs = []
-        
+    # Desc: get all VPC
+    def get_vpcs(self):
+        print(colored('Checking VPCs', 'yellow')) 
+        all_vpcs = []
         for region_info in boto3.client('ec2').describe_regions()['Regions']:
             region_name = region_info['RegionName']
             ec2_client = boto3.client('ec2', region_name=region_name)
             vpcs = ec2_client.describe_vpcs()['Vpcs']
-
             for vpc in vpcs:
                 vpc_info = {
                     "Region": region_name,
@@ -73,30 +140,23 @@ class Core:
                     "State": vpc['State'],
                     "Is Default": vpc['IsDefault']
                 }
-                allVpcs.append(vpc_info)
-
-        return allVpcs
+                all_vpcs.append(vpc_info)
+        return all_vpcs
     
-    def listLambda(self):
-        print(colored('Checking Lambda', 'yellow'))  # Yellow for visibility
-        
-        allLambda = []
-       
-        for region_info in boto3.client('ec2').describe_regions()['Regions']:
-            region_name = region_info['RegionName']
-            lambda_client = boto3.client('lambda', region_name=region_name)
-
-            # List all Lambda functions in the current region
-            response = lambda_client.list_functions()
-
-            for function in response['Functions']:
-                functionName = function['FunctionName']
-
-                allLambda.append({
-                    'Region': region_name,
-                    'FunctionName': functionName
-                })
-
+    # Desc: list all lambda function
+    def list_lambda(self):
+        print(colored('Checking Lambda', 'yellow')) 
+        allLambda = ""
+        response = self.lambda_.list_functions()
+        for function in response['Functions']:
+            function_name = function['FunctionName']
+            function_runtime = function['Runtime']
+            function_handler = function['Handler']
+            function_memory = function['MemorySize']
+            function_timeout = function['Timeout']
+            function_role = function['Role']
+            allLambda += f"{function_name}, {function_runtime}, {function_handler}, {function_memory}, {function_timeout}, {function_role}\n"
+            print(f"{function_name}, {function_runtime}, {function_handler}, {function_memory}, {function_timeout}, {function_role}")
         return allLambda
     
 
--- main.py
+++ main.py
@@ -1,33 +1,86 @@
 from termcolor import colored
 from core import Core
+import datetime
 
-coreServices = Core()  # Highlighted in blue for clarity
-resources = coreServices.getAllResources()
+# Create an instance of the Core service
+core_service = Core() 
 
+# Get all resources from the Core service
+resources = core_service.get_all_resources()
+
+# Iterate over each resource
 for resource in resources:
-    # print(resource)
+    # If the resource is an EC2 instance
     if resource == 'ec2':
-        coreServices.getEC2Instances(['running'])  # Applied yellow for visibility
-        pass
+        print(colored('Checking EC2 instances ', 'yellow')) 
+        # Get all EC2 instances
+        core_service.get_all_ec2_instances() 
+        
+    # If the resource is an S3 bucket
     elif resource == 's3':
-        buckets = coreServices.listS3Buckets()
+        # Get all S3 buckets
+        buckets = core_service.get_s3_buckets()
+        # Prepare the header for the bucket information
+        bucket_information = "Bucket Name,Creation Date,Owner ID,Objects,Object Key,Object Size,Object Last Modified\n"
+        # Iterate over each bucket
         for bucket in buckets:
-            print(colored('🪣 Found: ', 'green') + bucket['Name'])  # Green for positive findings
-        pass
+            # Get the details of the bucket and append it to the bucket information
+            bucket_information += core_service.get_s3_buckets_with_details(bucket)
+        # Get the current timestamp
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        # Write the bucket information to an S3 bucket
+        core_service.put_object_to_S3(bucket_information, "vuongbach-checking-services", f"s3/{timestamp}/s3_bucket_info.csv")
+
+    # If the resource is an RDS instance
     elif resource == 'rds':
-        rdsInstances = coreServices.listRDSInstances()
-        for instance in rdsInstances:
-            status_indicator = colored('⚠️ (running)', 'red') if instance['DBInstanceStatus'] == 'available' else 'Instance:'
-            print(status_indicator, instance['DBInstanceIdentifier'], instance['DBInstanceClass'], instance['Engine'])
-        pass
+        # Get all RDS instances
+        rds_instances = core_service.get_rds_instances()
+        # Prepare the header for the RDS information
+        rds_informations = "Instance ID, Instance Type, DB Engine"
+        # Iterate over each RDS instance
+        for instance in rds_instances:
+            # Get the details of the RDS instance and append it to the RDS information
+            rds_informations += core_service.get_rds_instance_detail(instance)
+        # Get the current timestamp
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        # Write the RDS information to an S3 bucket
+        core_service.put_object_to_S3(
+            rds_informations,
+            "vuongbach-checking-services",
+            f"rds/{timestamp}/instance_info.csv"
+        )
+
+    # If the resource is a VPC
     elif resource == 'vpc-lattice':
-        vpcs = coreServices.listVPCs()
+        # Get all VPCs
+        vpcs = core_service.get_vpcs()
+        # Prepare the header for the VPC information
+        vpc_informations = "Region, VPC ID, CIDR Block, State, Is Default\n"
+        # Iterate over each VPC
         for vpc in vpcs:
+            # Get the details of the VPC and append it to the VPC information
+            vpc_informations += f"{vpc['Region']}, {vpc['VPC ID']}, {vpc['CIDR Block']}, {vpc['State']}, {vpc['Is Default']}\n"
             print(f"Region: {vpc['Region']}, VPC ID: {vpc['VPC ID']}, CIDR Block: {vpc['CIDR Block']}, State: {vpc['State']}, Is Default: {vpc['Is Default']}")
-        pass
-    if resource == 'lambda':
-        lambdas = coreServices.listLambda()
-        for lambdaList in lambdas:
-            print(f"Region: {lambdaList['Region']}, "
-                f"Function Name: {lambdaList['FunctionName']}")
-        pass
+        # Get the current timestamp
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        # Write the VPC information to an S3 bucket
+        core_service.put_object_to_S3(
+            vpc_informations,
+            "vuongbach-checking-services",
+            f"vpc/{timestamp}/vpc_info.csv"
+        )
+
+    # If the resource is a Lambda function
+    elif resource == 'lambda':
+        # List all Lambda functions
+        lambdas = core_service.list_lambda()
+        # Prepare the header for the Lambda information
+        lambda_informations = "Function Name, Runtime, Handler, Memory Size, Timeout, Role"
+        # Get the current timestamp
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        # Write the Lambda information to an S3 bucket
+        core_service.put_object_to_S3(
+            lambdas,
+            "vuongbach-checking-services",
+            f"lambda/{timestamp}/vpc_info.csv"
+        )
